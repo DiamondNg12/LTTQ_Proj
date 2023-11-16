7e48b85e33ad3fa2727743af6cc4004d74f9790a
DEE <pqdat1209@gmail.com>
2023-11-17 01:34:14 +0700

    Revert "dat commit"
    This reverts commit 3af8eb4ebcddfa94fc86730e87f0adecb6361138.

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@ namespace LTTQ_Proj
 {
     internal static class Program
     {
-        // dat test ne
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
