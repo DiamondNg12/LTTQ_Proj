3af8eb4ebcddfa94fc86730e87f0adecb6361138
DEE <pqdat1209@gmail.com>
2023-11-17 01:24:03 +0700

    dat commit

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ namespace LTTQ_Proj
 {
     internal static class Program
     {
+        // dat test ne
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
